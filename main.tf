35ef832e2cf217c41ec51a817a57d9ae73f246c0
Anil <aperiyed@cisco.com>
2021-11-24 12:11:17 +0530

    add feature block at begining

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 # Create a resource group if it doesn't exist
+
+provider "azurerm" {
+   features {}
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.resource_group_location
