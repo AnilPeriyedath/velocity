65c613021c03d2dd6cd7548d88ba094364478d72
Anil <aperiyed@cisco.com>
2021-12-01 22:02:53 +0530

    disable password11

--- main.tf
+++ main.tf
@@ -40,6 +40,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   size                = "Standard_F2"
   admin_username      = "adminuser"
   admin_password      = "P@$$w0rd1234!"
+  disable_password_authentication = false
   network_interface_ids = [
     azurerm_network_interface.example.id,
   ]
